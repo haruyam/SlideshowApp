73f843ccf1e64a958572ecbfe98cfdb892a8bb89
春山優太 <haruyamayuuta@haruyamayuutanomacbook-puro.local>
2018-07-02 22:40:19 +0900

    FirstkadaiApp

---
+++ FirstkadaiApp/FirstkadaiApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstkadaiApp
+//
+//  Created by 春山優太 on 2018/06/27.
+//  Copyright © 2018年 haruyam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ myplayground2.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var item:(String, Int) = ("milk" , 200)
+let (name, price) = item
+
+print(name)
+print(price)
+
